8f87402fbc72f55d2d46ad67cfe26c7bf74d6b73
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-04 15:49:12 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -34,3 +34,26 @@ CREATE UNIQUE INDEX idx_properties_property_id ON properties(property_id);
 CREATE INDEX idx_properties_name ON properties(property_name);
 CREATE INDEX idx_properties_location ON properties(location);
 CREATE INDEX idx_properties_price ON properties(price_per_night);
+
+
+--Measure the query performance before and after adding indexes using EXPLAIN or ANALYZE
+SELECT 
+    b.booking_id,
+    u.first_name,
+    p.property_name,
+    b.check_in_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+WHERE b.check_in_date BETWEEN '2025-07-01' AND '2025-07-31'
+ORDER BY b.check_in_date;
+-- User and property IDs for join efficiency
+CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+
+-- Index for date filter
+CREATE INDEX idx_bookings_check_in_date ON bookings(check_in_date);
+
+-- Optional: indexes for sorting and lookup
+CREATE INDEX idx_users_user_id ON users(user_id);
+CREATE INDEX idx_properties_property_id ON properties(property_id);
