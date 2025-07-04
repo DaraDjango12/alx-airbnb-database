9d3a5d27201964bea303ed96e91ef599d744194e
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-04 11:34:16 +0100

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,55 @@
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.user_id;
+
+
+SELECT 
+    properties.property_id,
+    properties.property_name,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
+
+
+-- Simulated FULL OUTER JOIN for MySQL
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM users
+LEFT JOIN bookings ON users.user_id = bookings.user_id
+
+UNION
+
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date
+FROM bookings
+LEFT JOIN users ON bookings.user_id = users.user_id;
