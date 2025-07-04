12ab9c33ebf7f4ec2afc591883fbb42cec6752c4
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-04 14:06:23 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,35 @@
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    b.check_in_date,
+    b.check_out_date,
+
+    -- User details
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    -- Property details
+    p.property_id,
+    p.property_name,
+    p.location,
+    p.price_per_night,
+
+    -- Payment details
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method,
+    pay.status
+
+FROM bookings b
+
+-- Join with users
+INNER JOIN users u ON b.user_id = u.user_id
+
+-- Join with properties
+INNER JOIN properties p ON b.property_id = p.property_id
+
+-- Join with payments
+INNER JOIN payments pay ON b.booking_id = pay.booking_id;
