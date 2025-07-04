0ef21a71eaffdc59e1b7df5d34914d8b7e725273
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-04 14:13:21 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -33,3 +33,72 @@ INNER JOIN properties p ON b.property_id = p.property_id
 
 -- Join with payments
 INNER JOIN payments pay ON b.booking_id = pay.booking_id;
+
+
+
+
+
+
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    b.check_in_date,
+    b.check_out_date,
+
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    p.property_id,
+    p.property_name,
+    p.location,
+    p.price_per_night,
+
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method,
+    pay.status
+
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+INNER JOIN payments pay ON b.booking_id = pay.booking_id
+
+-- WHERE clause with AND condition
+WHERE pay.status = 'completed'
+AND b.check_out_date < CURRENT_DATE;
+
+
+
+EXPLAIN
+SELECT 
+    b.booking_id,
+    b.booking_date,
+    b.check_in_date,
+    b.check_out_date,
+
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+
+    p.property_id,
+    p.property_name,
+    p.location,
+    p.price_per_night,
+
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.payment_method,
+    pay.status
+
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+INNER JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE pay.status = 'completed'
+AND b.check_out_date < CURRENT_DATE;
+
