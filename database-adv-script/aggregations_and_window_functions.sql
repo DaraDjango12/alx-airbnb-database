14808d1f885f00d2e0e58a89f56286398ef442bc
toluwalase oloba <100348104+DaraDjango12@users.noreply.github.com>
2025-07-04 15:55:52 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,3 +9,17 @@ FROM (
     FROM bookings
     GROUP BY property_id
 ) AS property_booking_counts;
+
+
+
+SELECT 
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts;
